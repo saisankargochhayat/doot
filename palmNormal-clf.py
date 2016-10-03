1ca90d41b00ef689122f51fa667c362361331668
Sandeep <sandeep.mahapatra.5@gmail.com>
2016-10-03 16:18:57 +0530

    split data test

--- palmNormal-clf.py
+++ palmNormal-clf.py
@@ -1,5 +1,7 @@
 from sklearn import tree
 
+data = []
+labels = []
 train_data = []
 train_labels = []
 test_data = []
@@ -8,21 +10,15 @@ test_labels = []
 with open("palmpositiondata.csv", 'r') as ppd:
     for line in ppd:
         attr = line.split(',')
-        train_data.append(map(float, attr[0:3]))
-        train_labels.append(attr[3][0:-1])
+        data.append(map(float, attr[0:3]))
+        labels.append(attr[3][0:-1])
+
+from sklearn.cross_validation import train_test_split
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size = .5)
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(train_data, train_labels)
-
-with open("palmpositiontest.csv", 'r') as ppt:
-    for line in ppt:
-        attr = line.split(',')
-        test_data.append(map(float, attr[0:3]))
-        test_labels.append(attr[3][0:-1])
-
 predictions = clf.predict(test_data)
-for i in range(len(predictions)):
-    print predictions[i], test_labels[i] 
 
 from sklearn.metrics import accuracy_score
 print accuracy_score(test_labels, predictions)
