0cb8bdff85416414cb771beea555c7020e379c2b
Aditya359 <laluprasad.lenka95@gmail.com>
2016-10-03 21:36:46 +0530

    added classifier code for dataset

---
+++ palmNormal-clf.py
@@ -0,0 +1,24 @@
+from sklearn import tree
+
+data = []
+labels = []
+train_data = []
+train_labels = []
+test_data = []
+test_labels = []
+
+with open("palmpositiondata.csv", 'r') as ppd:
+    for line in ppd:
+        attr = line.split(',')
+        data.append(map(float, attr[0:31]))
+        labels.append(attr[31][0:-1])
+
+from sklearn.cross_validation import train_test_split
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size = .5)
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_labels)
+predictions = clf.predict(test_data)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(test_labels, predictions)
