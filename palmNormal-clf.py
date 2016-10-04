34e8d1e3bae2803141c0bc259756a8c4f3f50576
Sandeep <sandeep.mahapatra.5@gmail.com>
2016-10-04 20:44:30 +0530

    use of diff classifier

--- palmNormal-clf.py
+++ palmNormal-clf.py
@@ -1,6 +1,6 @@
-from sklearn import svm
+# from sklearn import svm
 # from sklearn import tree
-# from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 # import pprint
 
 data = []
@@ -16,11 +16,17 @@ with open("a_f_train.csv", 'r') as ppd:
         data.append(map(float, attr[0:31]))
         labels.append(attr[31][0:-1])
 
-with open("a_f_test1.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        test_data.append(map(float, attr[0:31]))
-        test_labels.append(attr[31][0:-1])
+# with open("a_f_test.csv", 'r') as ppd:
+#     for line in ppd:
+#         attr = line.split(',')
+#         test_data.append(map(float, attr[0:31]))
+#         test_labels.append(attr[31][0:-1])
+
+# with open("a_f_test1.csv", 'r') as ppd:
+#     for line in ppd:
+#         attr = line.split(',')
+#         test_data.append(map(float, attr[0:31]))
+#         test_labels.append(attr[31][0:-1])
 
 # with open("a_f_test2.csv", 'r') as ppd:
 #     for line in ppd:
@@ -28,21 +34,23 @@ with open("a_f_test1.csv", 'r') as ppd:
 #         test_data.append(map(float, attr[0:31]))
 #         test_labels.append(attr[31][0:-1])
 
-# with open("a_f_test.csv", 'r') as ppd:
-#     for line in ppd:
-#         attr = line.split(',')
-#         test_data.append(map(float, attr[0:31]))
-#         test_labels.append(attr[31][0:-1])
+with open("a_f_test3.csv", 'r') as ppd:
+    for line in ppd:
+        attr = line.split(',')
+        test_data.append(map(float, attr[0:31]))
+        test_labels.append(attr[31][0:-1])
+
 
 #from sklearn.cross_validation import train_test_split
 #train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size = .5)
 
-clf = svm.SVC()
+clf = KNeighborsClassifier()
 clf.fit(data,labels)
 predictions = clf.predict(test_data)
 
-# for  i in range(len(predictions)):
-#     print predictions[i], test_labels[i]
+for  i in range(len(predictions)):
+    if predictions[i] != test_labels[i]:
+        print predictions[i], test_labels[i]
 
 #with open("data1", 'w') as _file:
 #        _file.write(pprint.pformat(data1))
