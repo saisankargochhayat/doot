3bef75d90c7bdba75175679ac2477d1a27b97ad5
Sandeep <sandeep.mahapatra.5@gmail.com>
2016-10-04 20:22:46 +0530

    used knn

--- palmNormal-clf.py
+++ palmNormal-clf.py
@@ -1,4 +1,5 @@
 from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 # import pprint
 
 data = []
@@ -14,6 +15,18 @@ with open("a_f_train.csv", 'r') as ppd:
         data.append(map(float, attr[0:31]))
         labels.append(attr[31][0:-1])
 
+with open("a_f_test1.csv", 'r') as ppd:
+    for line in ppd:
+        attr = line.split(',')
+        test_data.append(map(float, attr[0:31]))
+        test_labels.append(attr[31][0:-1])
+
+with open("a_f_test2.csv", 'r') as ppd:
+    for line in ppd:
+        attr = line.split(',')
+        test_data.append(map(float, attr[0:31]))
+        test_labels.append(attr[31][0:-1])
+
 with open("a_f_test.csv", 'r') as ppd:
     for line in ppd:
         attr = line.split(',')
@@ -23,9 +36,12 @@ with open("a_f_test.csv", 'r') as ppd:
 #from sklearn.cross_validation import train_test_split
 #train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size = .5)
 
-clf = tree.DecisionTreeClassifier()
+clf = KNeighborsClassifier()
 clf.fit(data,labels)
 predictions = clf.predict(test_data)
+
+# for  i in range(len(predictions)):
+#     print predictions[i], test_labels[i]
 
 #with open("data1", 'w') as _file:
 #        _file.write(pprint.pformat(data1))
