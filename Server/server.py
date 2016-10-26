7446c00abd5f6fba7dcf3a63c5ad7903caa7a870
Rishav Agarwal <rishav.159@gmail.com>
2016-10-26 16:26:46 +0530

    Updated file structure

--- Server/server.py
+++ Server/server.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.linear_model import SGDClassifier
 from sklearn import svm
+from data_loader import data_loader
 # import pprint
 
 data = []
@@ -14,176 +15,10 @@ knn_model= KNeighborsClassifier()
 svm_model = svm.SVC()
 sgd_model = SGDClassifier(loss="hinge", penalty="l2")
 dtree_model = tree.DecisionTreeClassifier()
-with open("master_data/Alice/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Alice/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Alice/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Alice/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-# ----------------------------Anisha----------------------------
-with open("master_data/Anisha/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Anisha/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Anisha/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Anisha/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
 
-# ----------------------------Asutosh----------------------------
-with open("master_data/Asutosh/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Asutosh/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Asutosh/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Asutosh/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
+# data_loader is a function that loads data from the given csv files
+data,labels = data_loader()
 
-# ----------------------------Rishav----------------------------
-with open("master_data/Rishav/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Rishav/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Rishav/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Rishav/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-
-# ----------------------------Sai----------------------------
-with open("master_data/Sai/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sai/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sai/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sai/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-
-# ----------------------------Sandy----------------------------
-with open("master_data/Sandy/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sandy/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sandy/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sandy/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-
-# ----------------------------Sohini----------------------------
-with open("master_data/Sohini/a_f.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sohini/g_m.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sohini/n_s.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/Sohini/t_y.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-
-
-knn_model.fit(data,labels)
-svm_model.fit(data,labels)
-sgd_model.fit(data,labels)
-dtree_model.fit(data,labels)
-
-
-
-with open("master_data/zairza.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
-with open("master_data/rude.csv", 'r') as ppd:
-    for line in ppd:
-        attr = line.split(',')
-        data.append(list(map(float, attr[0:31])))
-        labels.append(attr[31][0:-1])
 knn_model.fit(data,labels)
 svm_model.fit(data,labels)
 sgd_model.fit(data,labels)
