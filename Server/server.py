c37e677d5ca9265e3f819a988ed76b0b43dff0c6
Sandeep <sandeep.mahapatra.5@gmail.com>
2016-12-22 19:46:52 +0530

    server file changes

--- Server/server.py
+++ Server/server.py
@@ -1,13 +1,18 @@
-import subprocess
-from bottle import run, post, request, response, get, route
-from bottle import static_file
+from tornado import websocket, web, ioloop
+import tornado.escape
+from tornado import gen
+import tornado.httpserver
+import tornado.options
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.linear_model import SGDClassifier
 from sklearn import svm
-import pandas
-import numpy as np
-# import pprint
+from data_loader import data_loader
+import json
+import pprint
+from  tornado.escape import json_decode
+from  tornado.escape import json_encode
+# define("port", default=8080, help="run on the given port", type=int)
 
 data = []
 labels = []
@@ -18,11 +23,7 @@ sgd_model = SGDClassifier(loss="hinge", penalty="l2")
 dtree_model = tree.DecisionTreeClassifier()
 
 # data_loader is a function that loads data from the given csv files
-dataFrame = pandas.read_csv('../CSV_Data/dataset_4.csv')
-uniqueLabels = dataFrame['label'].unique()
-labels = dataFrame['label'].values
-data = dataFrame.drop('label',axis=1).values
-
+data,labels = data_loader()
 
 knn_model.fit(data,labels)
 svm_model.fit(data,labels)
@@ -30,35 +31,46 @@ sgd_model.fit(data,labels)
 dtree_model.fit(data,labels)
 print("Trained")
 
-@route('/',method='GET')
-def index():
-    return static_file('index.html',root='static/')
+class HomeHandler(web.RequestHandler):
+    def get(self):
+        self.render("static/index.html")
 
-@route('/predictor',method='GET')
-def predictor():
-    return static_file('predictor.html',root='static/')
+class Predictor(web.RequestHandler):
+    def get(self):
+        self.render("static/predictor.html")
 
-@route('/visualizer',method='GET')
-def predictor():
-    return static_file('visualizer.html',root='static/')
+class Visualizer(web.RequestHandler):
+    def get(self):
+        self.render("static/visualizer.html")
 
+class Predict(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
 
-@route('/static/<filename>',method='GET')
-def serve_static(filename):
-    return static_file(filename,root='static/')
-@route('/train',method = 'GET')
-def train():
-    return "Trained"
+    def open(self):
+        print("WebSocket opened")
 
-@route('/predict',method = 'POST')
-def predict():
-    test_data = request.json['ar'];
-    # print(test_data)
-    predictions = {};
-    predictions['knn'] = str(knn_model.predict(test_data)[0])
-    predictions['svm'] = str(svm_model.predict(test_data)[0])
-    predictions['sgd'] = str(sgd_model.predict(test_data)[0])
-    predictions['dtree'] = str(dtree_model.predict(test_data)[0])
-    # print(predictions)
-    return predictions
-run(host='localhost', port=8080, debug=True)
+    def on_message(self, message):
+        msg = json.loads(message)
+        predictions = {};
+        predictions['knn'] = str(knn_model.predict([msg])[0])
+        predictions['svm'] = str(svm_model.predict([msg])[0])
+        predictions['sgd'] = str(sgd_model.predict([msg])[0])
+        predictions['dtree'] = str(dtree_model.predict([msg])[0])
+        self.write_message(predictions)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+app = web.Application([
+    (r'/static/(.*)', web.StaticFileHandler, {'path': 'static/'}),
+    (r"/",HomeHandler),
+    (r"/predictor",Predictor),
+    (r"/visualizer",Visualizer),
+    (r"/ws",Predict),
+    ])
+
+if __name__ == '__main__':
+    app.listen(8080)
+    print("Listening at 127.0.0.1:8080")
+    ioloop.IOLoop.instance().start()
