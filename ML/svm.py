279ae7627826e4de615f9a974fccdb39b8233042
Rishav Agarwal <rishav.159@gmail.com>
2017-01-01 16:07:00 +0530

    Reverted svm file

--- ML/svm.py
+++ ML/svm.py
@@ -6,7 +6,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
 from helper import svm
 dataFrame = pandas.read_csv('../CSV_Data/dataset_6.csv')
-dataFrame = dataFrame[~dataFrame['label'].isin(['m','n'])]
 
 sum_acc = 0
 sum_confusion = [[0 for x in range(22)] for y in range(22)]
