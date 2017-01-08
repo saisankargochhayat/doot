0378b205e8f1b21121770af248f23022735be4b8
Sandeep <sandeep.mahapatra.5@gmail.com>
2017-01-08 22:38:41 +0530

    fixed all_model of with_set

--- ML/with_set/all_model.py
+++ ML/with_set/all_model.py
@@ -12,20 +12,21 @@ import sys
 sys.path.append(path)
 from helper import svm,sgd,lda,ridge,knn,dtree
 
-names = ['rishi']
+names = ['rishi','sandy']
 sets = [['a','m','n','s','t','q','o','g','x'],['b','e','c'],['h','k','u','v'],['d','r','p']]
 
 for name in names:
     for i in range(1,3):
         allData = pandas.read_csv('../../CSV_Data/set_experiment/'+name+'_'+str(i)+'.csv')
         feature_list = allData.columns.values
+        print(name+'_'+str(i)+'.csv')
         for curr_set in sets:
             svm_sum_acc = 0
             knn_sum_acc = 0
             lda_sum_acc = 0
             sgd_sum_acc = 0
             dtree_sum_acc = 0
-            for i in range(100):
+            for j in range(100):
                 svm_acc,svm_conf= svm.get_set_accuracy(allData,curr_set,feature_list)
                 knn_acc,knn_conf= knn.get_set_accuracy(allData,curr_set,feature_list)
                 dtree_acc,dtree_conf= dtree.get_set_accuracy(allData,curr_set,feature_list)
@@ -41,7 +42,6 @@ for name in names:
             lda_sum_acc = lda_sum_acc/100
             sgd_sum_acc = sgd_sum_acc/100
             dtree_sum_acc = dtree_sum_acc/100
-            print(name+'_'+str(i)+'.csv')
             print(curr_set)
             print("SVM : " + str(svm_sum_acc))
             print("KNN : " + str(knn_sum_acc))
--- ML/with_set/dtree.py
+++
@@ -1,7 +0,0 @@
-import pandas
-from sklearn.tree import DecisionTreeClassifier
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from helper import 
