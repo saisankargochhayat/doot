4a5e93797cab5d2b8709783349bf1e1260861ff3
Rishav Agarwal <rishav.159@gmail.com>
2017-01-13 19:14:41 +0530

    Set_divide updated to check all set_experminet dataset and assignment.txt updated

--- ML/set_divide/knn.py
+++ ML/set_divide/knn.py
@@ -5,22 +5,33 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
 import sys
-sys.path.insert(0,'/home/rishi/doot/ML')
+import os
+path=os.getcwd()
+
+path=path.strip('with_set')
+path=path.strip('set_divide')
+sys.path.append(path)
 from helper import svm,sgd,knn,dtree,lda
-master_data = pandas.read_csv('../../CSV_Data/dataset_6.csv')
-all_data = master_data
+import glob
+path = os.path.split(path)[0]
+path =path.strip('ML')
+path = path+'/CSV_Data/set_experiment'
+allfiles = glob.glob(path+"/*")
+
 setlist = [['a','m','n','s','t','o','g','q','x'],['b','e','c'],['h','k','u','v'],
 ['d','r','p'],['f'],['l'],['i'],['w'],['y']]
 set_name_list = ['set_'+str(x) for x in range(len(setlist))]
-for curr_set,i in zip(setlist,set_name_list):
-    all_data['label'][all_data['label'].isin(curr_set)] = i
+for filename in allfiles:
 
-sum_acc=0;
-sum_conf_matrix = [[0 for x in range(len(set_name_list))] for y in range(len(set_name_list))]
-for i in range(100):
-    acc , conf = knn.find_accuracy(all_data)
-    sum_acc = sum_acc + acc
-    sum_conf_matrix = np.add(sum_conf_matrix,conf)
+    all_data = pandas.read_csv(filename)
+    for curr_set,i in zip(setlist,set_name_list):
+        all_data['label'][all_data['label'].isin(curr_set)] = i
 
-print(np.divide(sum_conf_matrix,100))
-print(sum_acc/100)
+    sum_acc=0;
+    sum_conf_matrix = [[0 for x in range(len(set_name_list))] for y in range(len(set_name_list))]
+    for i in range(100):
+        acc , conf ,report= knn.find_accuracy(all_data)
+        sum_acc = sum_acc + acc
+        sum_conf_matrix = np.add(sum_conf_matrix,conf)
+    print(filename)
+    print(sum_acc/100)
