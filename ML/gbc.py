ed5db2df4287c588de357026f687a9ced4beab43
sohini roychoudhury <sohinirc45@gmail.cm>
2017-01-25 11:01:35 +0530

    2 New classifiers added

---
+++ ML/gbc.py
@@ -0,0 +1,43 @@
+import pandas
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing, svm
+from sklearn.ensemble import GradientBoostingClassifier
+
+model = GradientBoostingClassifier('n_estimators':1200, 'max_depth':3, 'subsample':0.5, 'learning_rate':0.01, 'random_state':3)
+dataFrame = pandas.read_csv('C:\Users\user\Desktop\python\dataset_7.csv')
+#print(dataFrame)
+target =np.array(dataFrame['label'])
+
+features = np.array(dataFrame.drop(['label'],1))
+
+sum = 0
+for i in range(10):
+    train_feature, test_feature, train_target, test_target = cross_validation.train_test_split(features, target, test_size=0.2,stratify=target)
+    model.fit(train_feature, train_target)
+    predict = model.predict(test_feature)
+    acc = accuracy_score(test_target,predict)
+    sum = sum+acc
+
+print('dataset 7:')
+print(sum/10)
+
+dataFrame = pandas.read_csv('C:\Users\user\Desktop\python\dataset_8.csv')
+#print(dataFrame)
+
+target =np.array(dataFrame['label'])
+
+features = np.array(dataFrame.drop(['label'],1))
+
+sum = 0
+for i in range(10):
+    train_feature, test_feature, train_target, test_target = cross_validation.train_test_split(features, target, test_size=0.2,stratify=target)
+    model.fit(train_feature, train_target)
+    predict = model.predict(test_feature)
+    acc = accuracy_score(test_target,predict)
+    sum = sum+acc
+
+print('dataset 8:')
+print(sum/10)
---
+++ ML/lr.py
@@ -0,0 +1,62 @@
+import pandas
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing, cross_validation, svm
+from sklearn import linear_model
+
+model = linear_model.LogisticRegression(C=1e5)
+dataFrame = pandas.read_csv('C:\Users\user\Desktop\python\dataset_7.csv')
+#print(dataFrame)
+
+target =np.array(dataFrame['label'])
+
+features = np.array(dataFrame.drop(['label'],1))
+
+sum = 0
+for i in range(10):
+    train_feature, test_feature, train_target, test_target = cross_validation.train_test_split(features, target, test_size=0.2,stratify=target)
+    model.fit(train_feature, train_target)
+    predict = model.predict(test_feature)
+    acc = accuracy_score(test_target,predict)
+    sum = sum+acc
+
+print('Dataset 7:')
+print(sum/10)
+
+dataFrame = pandas.read_csv('C:\Users\user\Desktop\python\dataset_8.csv')
+#print(dataFrame)
+
+target =np.array(dataFrame['label'])
+
+features = np.array(dataFrame.drop(['label'],1))
+
+sum = 0
+for i in range(10):
+    train_feature, test_feature, train_target, test_target = cross_validation.train_test_split(features, target, test_size=0.2,stratify=target)
+    model.fit(train_feature, train_target)
+    predict = model.predict(test_feature)
+    acc = accuracy_score(test_target,predict)
+    sum = sum+acc
+
+print('Dataset 8:')
+print(sum/10)
+
+dataFrame = pandas.read_csv('C:\Users\user\Desktop\python\dataset_7.csv')
+#print(dataFrame)
+
+target =np.array(dataFrame['label'])
+
+features = np.array(dataFrame.drop(['label'],1))
+
+sum = 0
+for i in range(10):
+    train_feature, test_feature, train_target, test_target = cross_validation.train_test_split(features, target, test_size=0.2,stratify=target)
+    model.fit(train_feature, train_target)
+    predict = model.predict(test_feature)
+    acc = accuracy_score(test_target,predict)
+    sum = sum+acc
+
+print('Dataset 7:')
+print(sum/10)
