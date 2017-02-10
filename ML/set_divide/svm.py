65f9c3d822209aaa997023081779e08f7918ec1d
Sandeep <sandeep.mahapatra.5@gmail.com>
2017-02-10 13:56:14 +0530

    change in set division

--- ML/set_divide/svm.py
+++ ML/set_divide/svm.py
@@ -15,23 +15,21 @@ from helper import svm,sgd,knn,dtree,lda
 import glob
 path = os.path.split(path)[0]
 path =path.strip('ML')
-path = path+'/CSV_Data/set_experiment'
-allfiles = glob.glob(path+"/*")
+# allfiles = glob.glob(path+"/*")
 
-setlist = [['a','m','n','s','t','o','g','q','x'],['b','e','c'],['h','k','u','v'],
-['d','r','p'],['f'],['l'],['i'],['w'],['y']]
+setlist = [['a','m','n','s','t','g','q'],['b','e','c','o'],['h','k','u','v'],
+['d','r','p'],['f'],['l'],['i'],['w'],['y'],['x']]
 set_name_list = ['set_'+str(x) for x in range(len(setlist))]
-for filename in allfiles:
+# for filename in allfiles:
+#
+all_data = pandas.read_csv(path+'/CSV_Data/dataset_8.csv')
+for curr_set,i in zip(setlist,set_name_list):
+    all_data['label'][all_data['label'].isin(curr_set)] = i
 
-    all_data = pandas.read_csv(filename)
-    for curr_set,i in zip(setlist,set_name_list):
-        all_data['label'][all_data['label'].isin(curr_set)] = i
-
-    sum_acc=0;
-    sum_conf_matrix = [[0 for x in range(len(set_name_list))] for y in range(len(set_name_list))]
-    for i in range(100):
-        acc , conf ,report= svm.find_accuracy(all_data)
-        sum_acc = sum_acc + acc
-        sum_conf_matrix = np.add(sum_conf_matrix,conf)
-    print(filename)
-    print(sum_acc/100)
+sum_acc=0;
+sum_conf_matrix = [[0 for x in range(len(set_name_list))] for y in range(len(set_name_list))]
+for i in range(100):
+    acc , conf = svm.find_accuracy(all_data)
+    sum_acc = sum_acc + acc
+    sum_conf_matrix = np.add(sum_conf_matrix,conf)
+print(sum_acc/100)
